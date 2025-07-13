daa10bfa92d64a0b3f822d2071a2ad8d40ee30b2
Timur Bakiiev <177857078+Tim124v@users.noreply.github.com>
2025-07-13 19:11:00 +0200

    fix

--- app/(private routes)/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/(private routes)/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { fetchNoteById } from "@/lib/api/clientApi";
 import Modal from "@/components/Modal/Modal";
